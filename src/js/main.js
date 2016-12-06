e33e170ba80612cf1bd1ba4bd469b6f0121ddf9d
cfen <chris.fenn@guardian.co.uk>
2016-12-06 18:03:37 +0000

    Added data test

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 import reqwest from 'reqwest'
 import mainHTML from './text/main.html!text'
 import share from './lib/share'
+import dataBase from './data/countries.json!json'
 
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 
 export function init(el, context, config, mediator) {
     el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
-
+    console.log(dataBase)
     reqwest({
         url: 'http://ip.jsontest.com/',
         type: 'json',
