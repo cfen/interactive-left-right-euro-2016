ada14e5589c51dbed25572fa5c4e2d4a22064e89
cfen <chris.fenn@guardian.co.uk>
2016-12-08 15:40:53 +0000

    Using mustache templating

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,99 @@
 import reqwest from 'reqwest'
+import _ from 'lodash'
+import Mustache from 'mustache'
+import iframeMessenger from './lib/iframeMessenger'
 import mainHTML from './text/main.html!text'
+import listHTML from './text/list.html!text'
+
+
 import share from './lib/share'
+
 import dataBase from './data/countries.json!json'
 
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
+var electionsArr;
 
 export function init(el, context, config, mediator) {
-    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
-    console.log(dataBase)
-    reqwest({
-        url: 'http://ip.jsontest.com/',
-        type: 'json',
-        crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
-    });
 
-    [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
-        var network = shareEl.getAttribute('data-network');
-        shareEl.addEventListener('click',() => shareFn(network));
-    });
+    iframeMessenger.enableAutoResize();
+    
+    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+    var tgt = el.querySelector('.test-msg')
+
+
+    buildFramework(dataBase, tgt)
+ 
 }
+
+
+function buildFramework(dataBase, tgt){
+    electionsArr = [];
+
+    _.each(dataBase, function(item){
+        var election = {}
+        election.Country = (item.Country)
+        election.Year = (item.Year)
+        election.Party = (item.Party)
+        electionsArr.push(election)
+    }) 
+
+    console.log(electionsArr)
+
+   // el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+    var person = {
+        firstName: "Christophe",
+        lastName: "Coenraets",
+        blogURL: "http://coenraets.org"
+    };
+
+    var data = {
+    elections: electionsArr 
+    };
+
+    var template = listHTML;
+    var tplOp = Mustache.to_html(template, data);
+   
+
+
+    tgt.innerHTML = tplOp
+
+}
+
+
+// electionsArr = [];
+
+
+
+
+//     _.each(dataBase, function(item){
+//         var election = {}
+//         election.Country = (item.Country)
+//         election.Year = (item.Year)
+//         election.Party = (item.Party)
+//         electionsArr.push(election)
+//     }) 
+
+//     console.log(electionsArr)
+
+//    // el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+//     var person = {
+//         firstName: "Christophe",
+//         lastName: "Coenraets",
+//         blogURL: "http://coenraets.org"
+//     };
+
+//     var data = {
+//     elections: electionsArr 
+//     };
+
+//     var template = "Employees:<ul>{{#elections}}" +
+//                                 "<li>{{Year}} {{Country}}</li>" +
+//                                 "{{/elections}}</ul>";
+//     var tplOp = Mustache.to_html(template, data);
+   
+
+
+//     el.innerHTML = tplOp
